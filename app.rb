5dacd932ad75daa80e1e757b31e4ad49bc1b7205
Harley McDowell <hmcdowell95@gmail.com>
2020-04-22 19:28:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ class App < Sinatra::Base
     @name = params["name"]
     @coach = params["coach"]
     @pg = params["pg"]
-    @sg = params["Shooting_Guard"]
-    @sf = params["Small_Forward"]
-    @pf = params["Power_Forward"]
-    @c = params["Center"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
     erb :team
   end
 end
